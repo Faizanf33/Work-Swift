3f6ebc6b4d07d6f40d1bc750e7b624214ff42f17
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2019-04-20 00:41:03 +0500

    Updated

--- Basics and functions/dictionary.swift
+++ Basics and functions/dictionary.swift
@@ -10,15 +10,15 @@ d[3] = "Thre"
 // replace old with new
 d[3] = "Three"
 print(d)
-//
-// // Iteration
+
+// Iteration
 for (k, v) in d {
     print(k, v)
 }
-//
+
 print(d.keys, d.values)
-//
-// // Dictionary within dictionary
+
+// Dictionary within dictionary
 let newDict = ["Old": d]
 print (newDict)
 print (type(of: newDict))
--- Basics and functions/mean.swift
+++ Basics and functions/mean.swift
@@ -1,9 +1,9 @@
-func arithmeticMean(_ nums: Double, _ num2: ) -> (Double, ){
+func arithmeticMean(_ nums: Double...) -> (Double){
 	var total: Double = 0
 	for num in nums {
 		total += num
 	}
-	return (total / Double(nums.count)), 
+	return (total / Double(nums.count)),
 }
 
 print(arithmeticMean(3.3, 4.4, 6.6, 8.8))
