fda1aab972a0bb6b352664c3c144a88ab70a3a4e
Faizan Ahmad <31237870+Faizanf33@users.noreply.github.com>
2020-07-13 01:42:56 +0500

    Update matrix.swift

--- Object oriented programming/matrix.swift
+++ Object oriented programming/matrix.swift
@@ -20,9 +20,9 @@ struct Matrix {
 
 var matrix = Matrix(2, 2)
 matrix[0, 0] = 1.1
-//matrix[0, 1] = 2.1
+matrix[0, 1] = 2.1
 matrix[1, 0] = 2.2
-//matrix[1, 1] = 1.2
+matrix[1, 1] = 1.2
 
 matrix[2, 0] = 0.0	//It's a wrong idea
 
